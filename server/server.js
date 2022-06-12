87b306e06c132e748081b257b6ec3ffd284e9f0b
Yosefha4 <73281960+Yosefha4@users.noreply.github.com>
2022-06-12 17:08:03 +0300

    add server - node files

---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mg = require('mailgun-js');
+
+dotenv.config();
+
+const mailgun = () =>
+  mg({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMIAN,
+  });
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/api/email', (req, res) => {
+  const { email,  message } = req.body;
+  mailgun()
+    .messages()
+    .send(
+      {
+        from: 'CalZone <CalZone.sce@gmail.com>',
+        to: `${email}`,
+        html: `<p>${message}</p>`,
+      },
+      (error, body) => {
+        if (error) {
+          console.log(error);
+          res.status(500).send({ message: 'Error in sending email' });
+        } else {
+          console.log(body);
+          res.send({ message: 'Email sent successfully' });
+        }
+      }
+    );
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
+});
